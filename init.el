1e7a9685855bb850fc383b70f8523c2be5564243
peyton <p8yton@gmail.com>
2014-11-04 18:57:23 +0000

    duplicate load-path entry

--- init.el
+++ init.el
@@ -111,7 +111,6 @@
  (autoload 'artist-mode "artist" "Enter artist-mode" t)
 
 ;; autocomplete
-(add-to-list 'load-path "~/.emacs.d/")
 ;; (require 'auto-complete-config)
 ;; (add-to-list 'ac-dictionary-directories "~/.emacs.d//ac-dict")
 ;; (ac-config-default)
